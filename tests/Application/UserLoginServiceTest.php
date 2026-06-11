96652f67f0c928532063474a6cf52208e6b0a6c9
Julen Gallo <gallo.161801@e.unavarra.es>
2026-06-11 19:27:57 +0200

    [rojo] - Creado test para que si un usuario no esta logueado lo loguee

--- tests/Application/UserLoginServiceTest.php
+++ tests/Application/UserLoginServiceTest.php
@@ -6,15 +6,19 @@ namespace UserLoginService\Tests\Application;
 
 use PHPUnit\Framework\TestCase;
 use UserLoginService\Application\UserLoginService;
+use UserLoginService\Domain\User;
 
 final class UserLoginServiceTest extends TestCase
 {
     /**
      * @test
      */
-    public function userIsLoggedIn()
+    public function userIsNotLoggedIn()
     {
         $userLoginService = new UserLoginService();
+        $user = new User("Usuario");
+
+        $result = $userLoginService->manualLogin($user);
 
         $this->assertEquals("user logged", $userLoginService->manualLogin());
     }
