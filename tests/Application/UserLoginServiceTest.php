818b90c8e4932ae328a264f66cd94d1b02a8d787
Julen Gallo <gallo.161801@e.unavarra.es>
2026-06-11 19:43:05 +0200

    [rojo] - Creado test para comprobar que si un usario ya esta logueado devuelve la excepción esperada

--- tests/Application/UserLoginServiceTest.php
+++ tests/Application/UserLoginServiceTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace UserLoginService\Tests\Application;
 
+use Exception;
 use PHPUnit\Framework\TestCase;
 use UserLoginService\Application\UserLoginService;
 use UserLoginService\Domain\User;
@@ -21,5 +22,21 @@ final class UserLoginServiceTest extends TestCase
         $result = $userLoginService->manualLogin($user);
 
         $this->assertEquals("user logged", $result);
+    }
+
+    /**
+     * @test
+     */
+    public function userIsLoggedIn()
+    {
+        $userLoginService = new UserLoginService();
+        $user = new User("Usuario");
+
+        $result = $userLoginService->manualLogin($user);
+
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage("User already logged in");
+
+        $result = $userLoginService->manualLogin($user);
     }
 }
